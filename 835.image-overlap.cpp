b8323756c3386757f3a0d63258186480d1ffa6c1
旭東 宋 <xudong.song.a6g@tw.denso.com>
2022-10-27 10:51:15 +0800

    835 Image Overlap

--- 835.image-overlap.cpp
+++ 835.image-overlap.cpp
@@ -7,8 +7,38 @@
 // @lc code=start
 class Solution {
 public:
+    void up(vector<vector<int>>& img1){
+
+    }
+    void down(vector<vector<int>>& img1){
+
+    }
+    void right(vector<vector<int>>& img1){
+
+    }
+    void left(vector<vector<int>>& img1){
+
+    }
+
+    void moving(vector<vector<int>>& img1, vector<vector<int>>& img2, int& ans){
+        int ans_tmp = 0;
+        for(int i = 0; i < img1.size(); i++){
+            for(int j = 0; j < img1[0].size(); i++){
+                if(img1[i][j] == 1 && img2[i][j] == 1)
+                    ans_tmp++;
+            }
+        }
+
+        if(ans < ans_tmp){
+            ans = ans_tmp;
+        }
+    }
+
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        
+        int ans;
+        moving(img1, img2, ans);
+
+        return ans;
     }
 };
 // @lc code=end
