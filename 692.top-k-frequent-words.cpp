84c555eae2f43458b03bacbd78f20d9952d4e3b4
旭東 宋 <xudong.song.a6g@tw.denso.com>
2022-11-01 12:18:16 +0800

    Exercise 2022/11/01

--- 692.top-k-frequent-words.cpp
+++ 692.top-k-frequent-words.cpp
@@ -5,10 +5,28 @@
  */
 
 // @lc code=start
+
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        
+        map<string, int> m;
+        vector<string> ans;
+
+        for(auto w:words){
+            if(m.find(w) != m.end()){
+                m[w]++;
+            }else{
+                m[w] = 1;
+            }
+        }
+
+        for(auto it:m){
+            if(it.second == 2){
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
     }
 };
 // @lc code=end
