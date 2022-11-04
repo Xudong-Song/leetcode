4864c5b25f80a1978b342517e64369428450cb90
旭東 宋 <xudong.song.a6g@tw.denso.com>
2022-11-04 16:55:59 +0800

    Exercies 2022/11/04 -4

---
+++ 172.factorial-trailing-zeroes.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=172 lang=cpp
+ *
+ * [172] Factorial Trailing Zeroes
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        /**
+         * @brief trailing(在後面、尾隨)
+         * 
+         * Solution: Time Complexity: O(logˇ5N)
+         *           Accetped, 0ms(100%), 6MB(29.03%)
+         * 1. All 0 trailing should comes from 10.
+         * 2. Therefore, we can count how many 5 we has meet.
+         *    How about 2? The number we meet 2 is far more than we meet 5.
+         * 3. Not enough, for example, when we meet 25*4, we will get 100.
+         *    Therefore, when we meet 5^2 * 2^2, we will get another 0 behind.
+         *    So on and so forth, 5^3 * 2^3, 5^4 * 2^4 ...
+         * 
+         */
+        int ans = 0;
+        for(int i = 5; i <= n; i*=5) ans += n/i;
+        return ans;
+    }
+};
+// @lc code=end
+
