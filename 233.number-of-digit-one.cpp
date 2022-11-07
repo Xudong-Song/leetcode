01d87d2b5ffa36f8192b013f530121f4321fcfef
旭東 宋 <xudong.song.a6g@tw.denso.com>
2022-11-07 09:59:44 +0800

    Wait

---
+++ 233.number-of-digit-one.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=233 lang=cpp
+ *
+ * [233] Number of Digit One
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countDigitOne(int n) {
+        
+        return 360;
+    }
+};
+// @lc code=end
+
