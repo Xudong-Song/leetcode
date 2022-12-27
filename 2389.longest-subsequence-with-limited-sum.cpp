67475bdd54e26f49fd3c156a553300b300c4724b
旭東 宋 <xudong.song.a6g@tw.denso.com>
2022-12-27 13:43:47 +0800

    Exercise 2022/12/27

---
+++ 2389.longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=2389 lang=cpp
+ *
+ * [2389] Longest Subsequence With Limited Sum
+ */
+
+// @lc code=start
+
+#define OPTIMIZIED
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        /**
+         * @brief 
+         * Solution: Prefix Sum and Search
+         * Time Complexity: O(nlogn + n*m)
+         * Accepted: 44ms(33.57%), 13.8MB(37.34%)
+         * 1. Sort the nums and record the Prefix Sum into tmp.
+         * 2. Search the queries from tmp list.
+         * 
+         * Optimizied:
+         * 1. Binary Search (upper_bound)
+         *    Time Complexity: O(nlogn + mlogn)
+         *    Accepted: 19ms(75.38%), 13.8MB(37.34%)
+         * 
+         */
+        vector<int> ans(queries.size(), 0);
+        vector<int> tmp(nums.size(), 0);
+
+        sort(nums.begin(), nums.end()); // O(nlogn)
+
+        tmp[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++){ // O(n)
+            tmp[i] = tmp[i-1] + nums[i];
+            //cout << tmp[i] << endl;
+        }
+
+#ifndef OPTIMIZIED // O(n*m)
+        for(int i = 0; i < queries.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(queries[i] < tmp[j]){
+                    ans[i] = j;
+                    break;
+                }else if(j == nums.size()-1){
+                    ans[i] = j+1;
+                    break;
+                }
+            }
+        }
+#else   // O(mlogn)
+        for(int i = 0; i < queries.size(); i++){
+            int index = 0;
+            index = (upper_bound(tmp.begin(), tmp.end(), queries[i])-tmp.begin());
+            ans[i] = index;
+        }
+#endif
+
+        return ans;
+    }
+};
+// @lc code=end
+
